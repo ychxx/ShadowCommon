a7f1c6b38341c833a63d20d711c537b3783d6d45
yc <563919364@qq.com>
2021-07-05 15:34:39 +0800

    1.添加flow创建

--- commonLib/src/main/java/com/yc/commonlib/base/YcRepository.kt
+++ commonLib/src/main/java/com/yc/commonlib/base/YcRepository.kt
@@ -1,9 +1,18 @@
 package com.yc.commonlib.base
+
+import com.yc.commonlib.extension.checkNet
+import com.yc.commonlib.net.YcResult
+import kotlinx.coroutines.channels.ProducerScope
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.channelFlow
+
 /**
  * Creator: yc
  * Date: 2021/6/15 18:27
  * UseDes:
  */
 open class YcRepository {
-
+    public fun <T> ycFlow(block: suspend ProducerScope<YcResult<T>>.() -> Unit): Flow<YcResult<T>> = channelFlow {
+        block()
+    }.checkNet()
 }
